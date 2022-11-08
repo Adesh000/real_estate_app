8de746d477923b4d779077cb60e637b59f7fbc1f
Adesh Nayak <adeshnayak6@gmail.com>
2022-11-08 06:17:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Filter from './Components/Filter';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Filter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Filter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Filter = () => {
+    return (
+        <div className="bg-slate-200">
+            <h1>Search Properties to rent</h1>
+        </div>
+    )
+}
+
+export default Filter;
